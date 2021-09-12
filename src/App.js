9753ebf41cdc7c95cbcd413ec6551f2093f1abb5
Foysal Mohammad <foysal@earlblue.com>
2021-09-12 11:43:29 +0100

    react modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import './App.css'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 function App() {
-  return <div className="App"></div>
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  return (
+    <div className="App">
+      <button onClick={() => setModalIsOpen(true)}>Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        shouldCloseOnOverlayClick={false}
+        style={{
+          overlay: { backgroundColor: 'grey' },
+          content: { color: 'orange' },
+        }}
+      >
+        <h2>Modal title</h2>
+        <p>Modal Body</p>
+
+        <button onClick={() => setModalIsOpen(false)}>close X</button>
+      </Modal>
+    </div>
+  )
 }
 
 export default App
