8475f12a4423b2b87ceea05b6f9087745660a6de
Foysal Mohammad <foysal@earlblue.com>
2021-09-12 14:39:18 +0100

    counterup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.css'
+import CountUp from 'react-countup'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <>
+      <div className="App">
+        <CountUp end={4000} duration={10} /> <br />
+        <CountUp end={4000} duration={5} />
+      </div>
+    </>
+  )
 }
 
 export default App
