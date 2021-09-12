e6eef6a9efea41b57d2313947cb14575ac4899c4
Foysal Mohammad <foysal@earlblue.com>
2021-09-12 16:34:51 +0100

    react-color-picker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import './App.css'
+import React, { useState } from 'react'
+
+import { SketchPicker } from 'react-color'
 
 function App() {
-  return <div className="App"></div>
+  const [color, setColor] = useState('#fff')
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  return (
+    <div>
+      <button
+        onClick={() =>
+          setShowColorPicker((showColorPicker) => !showColorPicker)
+        }
+      >
+        {showColorPicker ? 'close color picker' : 'pick a color'}
+      </button>
+      {showColorPicker && (
+        <SketchPicker
+          color={color}
+          onChange={(updatedColor) => setColor(updatedColor.hex)}
+        />
+      )}
+
+      <h2>You picked {color}</h2>
+    </div>
+  )
 }
 
 export default App
