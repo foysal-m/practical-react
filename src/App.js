3a2f38835a8aa10f9e7edd51a3e9f9150a868c34
Foysal Mohammad <foysal@earlblue.com>
2021-09-13 15:11:50 +0100

    chart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import './App.css'
+import LineChart from './LineChart'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="chart">
+        <LineChart />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/LineChart.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+function LineChart() {
+  const data = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    datasets: [
+      {
+        label: 'Sales for 2020 (M)',
+        data: [3, 2, 2, 1, 5],
+      },
+    ],
+  }
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  )
+}
+
+export default LineChart
