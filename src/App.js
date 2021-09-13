5420fb2c69313dfe09d7a2727749d66099be4c8a
Foysal Mohammad <foysal@earlblue.com>
2021-09-13 11:24:33 +0100

    year drop down

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css'
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
 
+import 'react-datepicker/dist/react-datepicker.css'
 function App() {
-  return <div className="App"></div>
+  const [startDate, setStartDate] = useState(new Date())
+  return (
+    <div className="App">
+      {' '}
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="dd/MM/yyy"
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+        showYearDropdown
+        scrollableYearDropdown
+      />
+    </div>
+  )
 }
 
 export default App
