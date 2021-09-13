762453bbca9e064ec4a69aa80ebcfd9f611c8ee0
Foysal Mohammad <foysal@earlblue.com>
2021-09-13 12:59:57 +0100

    react video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css'
+import React from 'react'
+import ReactPlayer from 'react-player'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <ReactPlayer
+        width="480px"
+        height="280px"
+        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        controls
+        onReady={() => console.log('ready')}
+        onPlay={() => console.log('started')}
+        onPause={() => console.log('paused')}
+        onEnded={() => console.log('Ended')}
+        onError={() => console.log('error')}
+      />
+    </div>
+  )
 }
 
 export default App
